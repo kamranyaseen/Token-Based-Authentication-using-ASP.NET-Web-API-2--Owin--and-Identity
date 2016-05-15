85edb8e5e98e2973e169e8c901b963a5356c011f
kamranyaseen <kamranmyaseen@gmail.com>
2016-05-15 16:59:56 +0300

    Token Based Authentication using ASP.NET Web API 2, Owin, and Identity
    ASP.NET Web API 2, Owin, and Identity

---
+++ AngularJSAuthentication.API/Controllers/OrdersController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AngularJSAuthentication.API.Controllers
+{
+    [RoutePrefix("api/Orders")]
+    public class OrdersController : ApiController
+    {
+        [Authorize]
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            return Ok(Order.CreateOrders());
+        }
+
+    }
+
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public string CustomerName { get; set; }
+        public string ShipperCity { get; set; }
+        public Boolean IsShipped { get; set; }
+
+        public static List<Order> CreateOrders()
+        {
+            List<Order> OrderList = new List<Order> 
+            {
+                new Order {OrderID = 10248, CustomerName = "Imran Hafeez", ShipperCity = "Amman", IsShipped = true },
+                new Order {OrderID = 10249, CustomerName = "Iftkhar chowdary", ShipperCity = "Dubai", IsShipped = false},
+                new Order {OrderID = 10250,CustomerName = "Kamran Yasen", ShipperCity = "Jeddah", IsShipped = false },
+                new Order {OrderID = 10251,CustomerName = "Abu Baker Bashraheel", ShipperCity = "Abu Dhabi", IsShipped = false},
+                new Order {OrderID = 10252,CustomerName = "Shawqi Alhashdi", ShipperCity = "Kuwait", IsShipped = true}
+            };
+
+            return OrderList;
+        }
+    }
+}
---
+++ AngularJSAuthentication.API/Model/UserModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AngularJSAuthentication.API.Model
+{
+    public class UserModel
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+
+        //[Required]
+        //[StringLength(100, ErrorMessage = "The Email Address is not valid.")]
+        //[DataType(DataType.EmailAddress)]
+        //[Display(Name = "Email")]
+        //public string Email { get; set; }
+
+        //[Required]
+        //[StringLength(100, ErrorMessage = "The Confirm Email Address is not valid.")]
+        //[DataType(DataType.EmailAddress)]
+        //[Display(Name = "EmailConfirmed")]
+        //public string EmailConfirmed { get; set; }
+
+        //[Required]
+        //[StringLength(100, ErrorMessage = "The phone number is not valid.")]
+        //[DataType(DataType.PhoneNumber)]
+        //[Display(Name = "PhoneNumber")]
+        //public string PhoneNumber { get; set; }
+
+        //[Required]
+        //[StringLength(100, ErrorMessage = "The confirm phone number is not valid.")]
+        //[DataType(DataType.PhoneNumber)]
+        //[Display(Name = "PhoneNumberConfirmed")]
+        //public string PhoneNumberConfirmed { get; set; }
+    }
+}
